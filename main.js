8fd255967ca349428b435204f546c756ad8c9f94
Adam Shelley <adam.shelley18@gmail.com>
2020-02-20 20:00:31 +0000

    Improved visuals

--- main.js
+++ main.js
@@ -1,81 +1,82 @@
-// Secret key to access API
-const key = "757699383254d35cf4ebae4dd19eb9fda9049e52f394f409aad91358d23a9aea";
-
-
 //////////////////////////////////
 // Call API and get pictures
 //////////////////////////////////
-
 const getSearch = async calmInput => {
-    const response = await fetch(
-        `https://api.unsplash.com/search/photos?query=${calmInput}&client_id=${key}`
-    );
-    data = await response.json();
-    displayImages(data);
+  console.log("fetching " + calmInput);
+  const response = await fetch(
+    `https://api.unsplash.com/search/photos?query=${calmInput}&client_id=${key}`
+  );
+  data = await response.json();
+  displayImages(data);
 
-    // console.log(data.results);
+  // console.log(data.results);
 };
 
 //////////////////////////////////
 // Function to show the images gathered from the API call
 //////////////////////////////////
-
 function displayImages(data) {
-    
-    let colour = data.results[0].color;
-    let gradient = `linear-gradient(to bottom, ${colour},${data.results[1].color}, ${data.results[2].color})`;
-    const topContainer = document.querySelector('.main__container');
-    topContainer.style.background = `${colour}`;
-    console.log(colour);
+  let colour = data.results[0].color;
+  let gradient = `linear-gradient(to bottom, ${colour},${data.results[1].color}, ${data.results[2].color})`;
+  const topContainer = document.querySelector(".main__container");
+  topContainer.style.background = `${colour}`;
+  console.log(colour);
 
-    const imageContainer = document.querySelector(".main__container--images");
-    for (let i = 0; i < 10; i++) {
-        // How to insert colour?
-        imageContainer.style.background = gradient;
-        const img = document.createElement("img");
-        // let num = Math.floor(Math.random() * 10) + 1;
-        let image = `${data.results[i]["urls"]["raw"]}&w=800&h=800&dpi=2`;
-        img.src = image;
-        imageContainer.appendChild(img);
-    }
+  const imageContainer = document.querySelector(".main__container--images");
+  for (let i = 0; i < 10; i++) {
+    // How to insert colour?
+    imageContainer.style.background = gradient;
+    const img = document.createElement("img");
+    // let num = Math.floor(Math.random() * 10) + 1;
+    let image = `${data.results[i]["urls"]["raw"]}&w=800&h=800&dpi=2`;
+    img.src = image;
+    imageContainer.appendChild(img);
+  }
 }
 
-// window.addEventListener('DOMContentLoaded', getSearch);
+///////////////////////////////////
+// Clear images function
+//////////////////////////////////
+function clearImages() {
+  const imageContainer = document.querySelector(".main__container--images");
+  while (imageContainer.firstChild) {
+    imageContainer.removeChild(imageContainer.firstChild);
+  }
+}
 
 ///////////////////////////////////
 // Event listener on submit button
 //////////////////////////////////
-
 document.querySelector("#calm-form").addEventListener("submit", e => {
-    e.preventDefault();
-    // Get form values
-    //Remove existing pictures
-    const imageContainer = document.querySelector(".main__container--images");
-    while (imageContainer.firstChild) {
-        imageContainer.removeChild(imageContainer.firstChild);
-    }
-    // Search for new pictures
-    const calmInput = document.querySelector("#calm");
+  e.preventDefault();
+  // Get form values
+  //Remove existing pictures
+  clearImages();
+  // Search for new pictures
+  const calmInput = document.querySelector("#calm");
 
-    if (calmInput.value === "") {
-        getSearch("calm");
-    } else {
-        getSearch(calmInput.value);
-    }
+  if (calmInput.value === "") {
+    getSearch("calm");
+  } else {
+    getSearch(calmInput.value);
+  }
 
-    calmInput.value = "";
-    moveNav();
-    // Move h1 + input boxes to top
+  calmInput.value = "";
 });
 
 ///////////////////////////////////
-// Move the nav bar upwards to give more space for pictures
+// Event Listeners on the buttons
 //////////////////////////////////
 
-function moveNav() {
-    const navContainer = document.querySelector('.main__container--nav-container');
-    navContainer.classList.add('nav-clicked-transition');
-    
-    const navBar = document.querySelector('.main__container--nav-container');
-    navBar.classList.add('navbar-smaller');
-}
+const ideas = document.querySelectorAll(".idea");
+
+ideas.forEach(idea => {
+  idea.addEventListener("click", e => {
+    // Remake
+    clearImages();
+    getSearch(e.target.innerHTML);
+  });
+});
+
+// Secret key to access API
+const key = "757699383254d35cf4ebae4dd19eb9fda9049e52f394f409aad91358d23a9aea";
